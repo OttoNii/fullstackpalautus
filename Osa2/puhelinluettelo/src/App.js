1df7cb947498ba3145675f67e6198a6875a6b0cb
OttoNii <otto.niittykoski@gmail.com>
2023-08-09 13:07:55 +0300

    Puhelinluettelo step2

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
- 
+const Person = ({ person }) => {
+  return <li>{person.name}</li>;
+};
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -8,19 +10,31 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
-    const newPerson = {name: newName}
-    setPersons([... persons, newPerson])
-    setNewName('')
+    const nameExists = persons.some(person => person.name === newName);
+    if (nameExists) {
+      window.alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPerson = { name: newName };
+      setPersons([...persons, newPerson]);
+      setNewName('');
+    }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input 
+          value={newName}
+          onChange={handleNoteChange}/>
         </div>
         <div>
           <button type="submit">add</button>
