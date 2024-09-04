ef2296211f8b48d1b3b2c2f6390807e031bf1afb
OttoNii <otto.niittykoski@gmail.com>
2024-09-04 13:04:22 +0300

    4.4

--- Osa4/utils/list_helper.js
+++ Osa4/utils/list_helper.js
@@ -1,7 +1,11 @@
 const dummy = (blogs) => {
     return 1
   }
-  
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
   module.exports = {
+    totalLikes,
     dummy
   }
+
